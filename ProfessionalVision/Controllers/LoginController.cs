74a22d121a6e5b0be0f7b92916926c8a76ceb098
Muhammad Saad Hassan <saad41mgc@hotmail.com>
2020-02-29 14:47:35 +0800

    push2

---
+++ ProfessionalVision/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProfessionalVision.Controllers
+{
+    public class LoginController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }  
+    }
+}
